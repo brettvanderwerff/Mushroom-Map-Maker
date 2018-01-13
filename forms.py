97a43239c76229555a62263a3732157b70258f42
brettvanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-01-13 13:53:15 -0800

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class UserInput(FlaskForm):
-    genus_name = StringField('Genus name: ', validators=[DataRequired('Please enter a genus name')])
-    species_name = StringField('Species name: ', validators=[DataRequired('Please enter a species name')])
-    submit = SubmitField('Sign In')
