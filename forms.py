2b6ae233e60c811eafb9d60f8435b5850d29135a
brettvanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-01-13 15:33:46 -0800

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,13 @@
+'''A UserInput class is generated for downstream import and instantiation by the mushroom_map_maker_web_app module.'''
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+'''
+UserInput class built around flask-wtf will be used to generate a form to accept user input in the form of a mushroom
+genus and species.'''
+class UserInput(FlaskForm):
+    genus_name = StringField('Genus name: ', validators=[DataRequired('Please enter a genus name.')])
+    species_name = StringField('Species name: ', validators=[DataRequired('Please enter a species name.')])
+    submit = SubmitField('Submit')
